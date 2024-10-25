2712712fd90604734f7be173123ac8942896fe56
jadenalexle@gmail.com <jadenalexle@gmail.com>
2024-10-24 17:32:21 -0700

    tof sensor code

--- esp32_s3_devkitc/src/main.cpp
+++ esp32_s3_devkitc/src/main.cpp
@@ -1,56 +1,63 @@
 #include <Arduino.h>
-#include <HCSR04.h>            // HCSR04 ultrasonic sensor@^2.0.3 library
 #include <Adafruit_NeoPixel.h> // onboard RGB library
 
-// define allocates to Flash Memory (non-volatile)
+#include <Wire.h>
+#include <VL53L1X.h>
 
+VL53L1X sensor;
+
+// define allocates to Flash Memory (non-volatile)
 #define PIN 38 // RGB
 #define NUMPIXELS 1
 
-// sensor 1
-const uint8_t trig1 = 6;
-const uint8_t echo1 = 7;
-
-// sensor 2
-const uint8_t trig2 = 12;
-const uint8_t echo2 = 11;
+// Set SCL and SDA pins
+#define SCL_PIN 9 // Set pin 9 as SCL pin
+#define SDA_PIN 10 // Set pin 10 as SDA pin
 
 // onboard RGB setup
 Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
-// setting up both sensors
-HCSR04 sensor1(trig1, echo1);
-HCSR04 sensor2(trig2, echo2);
-
 // Threshold distance in cm (triggers green color)
-const float THRESHOLD = 10.0;
+const float THRESHOLD_MIN = 300.0; // 0.3 meters
+const float THRESHOLD_MAX = 1500.0; // 1.5 meters
 
 void setup()
 {
+  // while (!Serial) {}
+  Wire.begin(SDA_PIN, SCL_PIN);
+
+  sensor.setTimeout(500);
+  if (!sensor.init())
+  {
+    Serial.println("Failed to detect and initialize sensor!");
+    while (1);
+  }
+  // Use long distance mode and allow up to 50000 us (50 ms) for a measurement.
+  // You can change these settings to adjust the performance of the sensor, but
+  // the minimum timing budget is 20 ms for short distance mode and 33 ms for
+  // medium and long distance modes. See the VL53L1X datasheet for more
+  // information on range and timing limits.
+  sensor.setDistanceMode(VL53L1X::Long);
+  sensor.setMeasurementTimingBudget(50000);
+
+  // Start continuous readings at a rate of one measurement every 50 ms (the
+  // inter-measurement period). This period should be at least as long as the
+  // timing budget.
+  sensor.startContinuous(50);
+
   Serial.begin(9600);
   pixels.begin();
 }
 
 void loop()
 {
-  // get distance measurements
-  float dist1 = sensor1.dist();
-  float dist2 = sensor2.dist();
-
-  // print statements for debugging
-  Serial.print("sensor 1: ");
-  Serial.println(dist1);
-  Serial.print("sensor 2: ");
-  Serial.println(dist2);
+  const float distance = sensor.read();
+  Serial.println(distance);
 
   // if object is within the THRESHOLD
-  if (dist1 <= THRESHOLD && dist2 <= THRESHOLD)
-  {
-    Serial.println("THRESHOLD MET!");
+  if (distance <= THRESHOLD_MAX && distance >= THRESHOLD_MIN) {
     pixels.setPixelColor(0, pixels.Color(0, 255, 0)); // GREEN
-  }
-  else
-  {
+   } else {
     pixels.setPixelColor(0, pixels.Color(255, 0, 0)); // RED
   }
   pixels.show();
