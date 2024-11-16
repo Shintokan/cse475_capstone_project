bfc127259750e0814d67ec84b2f3879b3befbf4b
shimakan000 <kandai2003@gmail.com>
2024-11-15 22:38:58 -0800

    model implementation

--- esp32_sense/src/main.cpp
+++ esp32_sense/src/main.cpp
@@ -1,35 +1,13 @@
-#define CAMERA_MODEL_XIAO_ESP32S3 // Has PSRAM
-
 /* Includes ---------------------------------------------------------------- */
 #include <haptic-cane-new-model_inferencing.h>
 #include "edge-impulse-sdk/dsp/image/image.hpp"
-
 #include "esp_camera.h"
+
+#define CAMERA_MODEL_XIAO_ESP32S3 // Has PSRAM
+#include "camera_pins.h"
 
 // Select camera model - find more camera models in camera_pins.h file here
 // https://github.com/espressif/arduino-esp32/blob/master/libraries/ESP32/examples/Camera/CameraWebServer/camera_pins.h
-
-#define CAMERA_MODEL_XIAO_ESP32S3 // Has PSRAM
-
-#define PWDN_GPIO_NUM -1
-#define RESET_GPIO_NUM -1
-#define XCLK_GPIO_NUM 10
-#define SIOD_GPIO_NUM 40
-#define SIOC_GPIO_NUM 39
-
-#define Y9_GPIO_NUM 48
-#define Y8_GPIO_NUM 11
-#define Y7_GPIO_NUM 12
-#define Y6_GPIO_NUM 14
-#define Y5_GPIO_NUM 16
-#define Y4_GPIO_NUM 18
-#define Y3_GPIO_NUM 17
-#define Y2_GPIO_NUM 15
-#define VSYNC_GPIO_NUM 38
-#define HREF_GPIO_NUM 47
-#define PCLK_GPIO_NUM 13
-
-#define LED_GPIO_NUM 21
 
 /* Constant defines -------------------------------------------------------- */
 #define EI_CAMERA_RAW_FRAME_BUFFER_COLS 320
@@ -130,75 +108,92 @@ void setup()
  */
 void loop()
 {
-
-  // instead of wait_ms, we'll wait on the signal, this allows threads to cancel us...
-  if (ei_sleep(5) != EI_IMPULSE_OK)
+  if (Serial.available())
   {
-    return;
-  }
+    String command = Serial.readStringUntil('\n');
+    command.trim();
+    if (command == "c")
+    {
+      snapshot_buf = (uint8_t *)malloc(EI_CAMERA_RAW_FRAME_BUFFER_COLS * EI_CAMERA_RAW_FRAME_BUFFER_ROWS * EI_CAMERA_FRAME_BYTE_SIZE);
 
-  snapshot_buf = (uint8_t *)malloc(EI_CAMERA_RAW_FRAME_BUFFER_COLS * EI_CAMERA_RAW_FRAME_BUFFER_ROWS * EI_CAMERA_FRAME_BYTE_SIZE);
+      // check if allocation was successful
+      if (snapshot_buf == nullptr)
+      {
+        ei_printf("ERR: Failed to allocate snapshot buffer!\n");
+        return;
+      }
 
-  // check if allocation was successful
-  if (snapshot_buf == nullptr)
-  {
-    ei_printf("ERR: Failed to allocate snapshot buffer!\n");
-    return;
-  }
+      ei::signal_t signal;
+      signal.total_length = EI_CLASSIFIER_INPUT_WIDTH * EI_CLASSIFIER_INPUT_HEIGHT;
+      signal.get_data = &ei_camera_get_data;
 
-  ei::signal_t signal;
-  signal.total_length = EI_CLASSIFIER_INPUT_WIDTH * EI_CLASSIFIER_INPUT_HEIGHT;
-  signal.get_data = &ei_camera_get_data;
+      if (ei_camera_capture((size_t)EI_CLASSIFIER_INPUT_WIDTH, (size_t)EI_CLASSIFIER_INPUT_HEIGHT, snapshot_buf) == false)
+      {
+        ei_printf("Failed to capture image\r\n");
+        free(snapshot_buf);
+        return;
+      }
 
-  if (ei_camera_capture((size_t)EI_CLASSIFIER_INPUT_WIDTH, (size_t)EI_CLASSIFIER_INPUT_HEIGHT, snapshot_buf) == false)
-  {
-    ei_printf("Failed to capture image\r\n");
-    free(snapshot_buf);
-    return;
-  }
+      // Run the classifier
+      ei_impulse_result_t result = {0};
 
-  // Run the classifier
-  ei_impulse_result_t result = {0};
+      EI_IMPULSE_ERROR err = run_classifier(&signal, &result, debug_nn);
+      if (err != EI_IMPULSE_OK)
+      {
+        ei_printf("ERR: Failed to run classifier (%d)\n", err);
+        return;
+      }
 
-  EI_IMPULSE_ERROR err = run_classifier(&signal, &result, debug_nn);
-  if (err != EI_IMPULSE_OK)
-  {
-    ei_printf("ERR: Failed to run classifier (%d)\n", err);
-    return;
-  }
-
-  // print the predictions
-  ei_printf("Predictions (DSP: %d ms., Classification: %d ms., Anomaly: %d ms.): \n",
-            result.timing.dsp, result.timing.classification, result.timing.anomaly);
+      // print the predictions
+      ei_printf("Predictions (DSP: %d ms., Classification: %d ms., Anomaly: %d ms.): \n",
+                result.timing.dsp, result.timing.classification, result.timing.anomaly);
 
 #if EI_CLASSIFIER_OBJECT_DETECTION == 1
-  bool bb_found = result.bounding_boxes[0].value > 0;
-  for (size_t ix = 0; ix < result.bounding_boxes_count; ix++)
-  {
-    auto bb = result.bounding_boxes[ix];
-    if (bb.value == 0)
-    {
-      continue;
-    }
-    ei_printf("    %s (%f) [ x: %u, y: %u, width: %u, height: %u ]\n", bb.label, bb.value, bb.x, bb.y, bb.width, bb.height);
-  }
-  if (!bb_found)
-  {
-    ei_printf("    No objects found\n");
-  }
+      // bool bb_found = result.bounding_boxes[0].value > 0;
+      bool object_detected = false;
+      for (size_t ix = 0; ix < result.bounding_boxes_count; ix++)
+      {
+        auto bb = result.bounding_boxes[ix];
+        if (bb.value > 0.2)
+        {
+          // if (bb.value == 0)
+          // {
+          //   continue;
+          // }
+          object_detected = true;
+          ei_printf(" %s: %.5f\n", bb.label, bb.value);
+        }
+        // ei_printf("    %s (%f) [ x: %u, y: %u, width: %u, height: %u ]\n", bb.label, bb.value, bb.x, bb.y, bb.width, bb.height);
+      }
+      if (!object_detected)
+      {
+        ei_printf("No objects found\n");
+      }
 #else
-  for (size_t ix = 0; ix < EI_CLASSIFIER_LABEL_COUNT; ix++)
-  {
-    ei_printf("    %s: %.5f\n", result.classification[ix].label,
-              result.classification[ix].value);
-  }
+      for (size_t ix = 0; ix < EI_CLASSIFIER_LABEL_COUNT; ix++)
+      {
+        ei_printf("    %s: %.5f\n", result.classification[ix].label,
+                  result.classification[ix].value);
+      }
 #endif
 
 #if EI_CLASSIFIER_HAS_ANOMALY == 1
-  ei_printf("    anomaly score: %.3f\n", result.anomaly);
+      ei_printf("    anomaly score: %.3f\n", result.anomaly);
 #endif
 
-  free(snapshot_buf);
+      free(snapshot_buf);
+    }
+    else if (command == "q")
+    {
+      Serial.println("Exiting...");
+      while (1)
+        ; // Halt the program
+    }
+    else if (!command.isEmpty())
+    {
+      Serial.println("Invalid command. Enter 'c' to capture and run inference, or 'q' to quit.");
+    }
+  }
 }
 
 /**
@@ -208,14 +203,8 @@ void loop()
  */
 bool ei_camera_init(void)
 {
-
   if (is_initialised)
     return true;
-
-#if defined(CAMERA_MODEL_ESP_EYE)
-  pinMode(13, INPUT_PULLUP);
-  pinMode(14, INPUT_PULLUP);
-#endif
 
   // initialize the camera
   esp_err_t err = esp_camera_init(&camera_config);
@@ -226,23 +215,6 @@ bool ei_camera_init(void)
   }
 
   sensor_t *s = esp_camera_sensor_get();
-  // initial sensors are flipped vertically and colors are a bit saturated
-  if (s->id.PID == OV3660_PID)
-  {
-    s->set_vflip(s, 1);      // flip it back
-    s->set_brightness(s, 1); // up the brightness just a bit
-    s->set_saturation(s, 0); // lower the saturation
-  }
-
-#if defined(CAMERA_MODEL_M5STACK_WIDE)
-  s->set_vflip(s, 1);
-  s->set_hmirror(s, 1);
-#elif defined(CAMERA_MODEL_ESP_EYE)
-  s->set_vflip(s, 1);
-  s->set_hmirror(s, 1);
-  s->set_awb_gain(s, 1);
-#endif
-
   is_initialised = true;
   return true;
 }
